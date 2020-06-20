1539b8899bfb68fd95134741b3ff1730528b17c2
Tiago Cardoso <tiagocardoso011@gmail.com>
2020-06-20 17:19:58 -0300

    Commit inicial
    Cara inicial do projeto

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    AjustaPreview();
+});
+
+
+function AjustaPreview(){
+    let urlMontada = "<h4><a href=https://github.com/sir-tiago/testeDeServicos target='_blank'>Caminho para o projeto</a></h4>";
+    $("#preview").append(urlMontada);
+}
